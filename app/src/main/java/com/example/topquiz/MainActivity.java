6f3172fc9eb5be5b045bd73a342be329bd750cae
Amal <amalsouheil@gmail.com>
2019-01-30 16:39:06 +0100

    création de la seconde activitée GameActivity la liaison des deux activitées MainActivity et GameActivity

--- app/src/main/java/com/example/topquiz/MainActivity.java
+++ app/src/main/java/com/example/topquiz/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.topquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -47,7 +48,8 @@ public class MainActivity extends AppCompatActivity {
               mPlayButton.setOnClickListener(new View.OnClickListener() {
                   @Override
                   public void onClick(View v) {
-                      
+                      Intent GameActivityIntent = new Intent(MainActivity.this,GameActivity.class);
+                      startActivity(GameActivityIntent);
                   }
               });
 
