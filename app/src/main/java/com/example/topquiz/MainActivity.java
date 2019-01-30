ee49ed6828d640b95b734d90d3434a6415995ca5
Amal <amalsouheil@gmail.com>
2019-01-30 14:23:29 +0100

    Référencement des éléments graphiques dans mon activité

--- app/src/main/java/com/example/topquiz/MainActivity.java
+++ app/src/main/java/com/example/topquiz/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.topquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView mGreetingText;
+    private EditText mNameInput;
+    private Button mPlayButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_txt);
+        mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
+        mPlayButton = (Button) findViewById(R.id.activity_main_play_btn);
+
     }
+
+
+
+
+
 }
