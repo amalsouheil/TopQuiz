f394a35845b1b2c32e69b21a650a3b357e650097
Amal <amalsouheil@gmail.com>
2019-01-31 10:47:16 +0100

    Mise a jour du controlleur GameActivity

--- app/src/main/java/com/example/topquiz/model/Question.java
+++ app/src/main/java/com/example/topquiz/model/Question.java
@@ -11,6 +11,42 @@ public class Question {
     private List<String> mChoiceList;
     private int mAnswerIndex;
 
+    public Question(String mQuestion, List<String> mChoiceList, int mAnswerIndex) {
+        this.mQuestion = mQuestion;
+        this.mChoiceList = mChoiceList;
+        this.mAnswerIndex = mAnswerIndex;
+    }
 
+    public String getQuestion() {
+        return mQuestion;
+    }
 
+    public void setQuestion(String mQuestion) {
+        this.mQuestion = mQuestion;
+    }
+
+    public List<String> getChoiceList() {
+        return mChoiceList;
+    }
+
+    public void setChoiceList(List<String> mChoiceList) {
+        this.mChoiceList = mChoiceList;
+    }
+
+    public int getAnswerIndex() {
+        return mAnswerIndex;
+    }
+
+    public void setAnswerIndex(int mAnswerIndex) {
+        this.mAnswerIndex = mAnswerIndex;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "mQuestion='" + mQuestion + '\'' +
+                ", mChoiceList=" + mChoiceList +
+                ", mAnswerIndex=" + mAnswerIndex +
+                '}';
+    }
 }
